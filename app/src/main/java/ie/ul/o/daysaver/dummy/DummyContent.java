6b8747a1d38ce22ebf01a10cf6db1053fb35d2e2
RookiePlays <36009712+RookiePlayers@users.noreply.github.com>
2018-04-23 22:15:04 +0100

    Add more Tutorial Videos

--- app/src/main/java/ie/ul/o/daysaver/dummy/DummyContent.java
+++ app/src/main/java/ie/ul/o/daysaver/dummy/DummyContent.java
@@ -39,12 +39,17 @@ public class DummyContent {
                 "https://firebasestorage.googleapis.com/v0/b/hours-1a681.appspot.com/o/tutorial_vids%2Fgym.mp4?alt=media&token=f01737b7-de1f-4419-bed1-5631fc79aa55","Gym •\n" +
                 "    \nDefault: select a body type, select a muscle group, hit save or change setting to your prefrence, hold the top for default option or tap to select all. When you are done hit finshed or if you need to change anything hit reset.\n" +
                 "    \nCustom: select one of your own workout or one from the community, you can add it to your calender if you want. Add your own plan by hitting the add Plan button. reload to see changes and Boom you're Done!"));
-        addItem(new DummyItem("4","How to use Social in 24H","https://firebasestorage.googleapis.com/v0/b/hours-1a681.appspot.com/o/tutorial_vids%2Fsocial.mp4?alt=media&token=62bb71a0-0da3-4ef9-8077-58a744bb5138","Select one of the provided Templates or Create Your Own via the circular Button Thingy, the rest is simple"));
-        addItem(new DummyItem("5","How to use Study in 24H","https://firebasestorage.googleapis.com/v0/b/hours-1a681.appspot.com/o/tutorial_vids%2Fstudy.mp4?alt=media&token=23c13f02-a883-4fc9-a806-6a9b76ea1e7c","Enter Your Subjects, select subjects to study hit the CHECK MARK AFTER EVERY DAY to save, the  blue button to save and close"));
-        addItem(new DummyItem("6","How to use Quick note in 24H","https://firebasestorage.googleapis.com/v0/b/hours-1a681.appspot.com/o/tutorial_vids%2Fqplan.mp4?alt=media&token=eced8d01-8592-422f-82fe-106ee78a5d1d",""));
-        addItem(new DummyItem("7","How to use Notes in 24H","https://firebasestorage.googleapis.com/v0/b/hours-1a681.appspot.com/o/tutorial_vids%2Fnotes.mp4?alt=media&token=9ff727b5-5eee-4a1f-a76f-86d74104ade7","Watch the Video!"));
-        addItem(new DummyItem("8","How to use Calendar in 24H","https://firebasestorage.googleapis.com/v0/b/hours-1a681.appspot.com/o/tutorial_vids%2Fcalendar.mp4?alt=media&token=8b8894bb-3fdc-4d35-8018-82dd1257b02c","enjoy!"));
-        addItem(new DummyItem("9","Home • 24H","https://firebasestorage.googleapis.com/v0/b/hours-1a681.appspot.com/o/tutorial_vids%2Fmain.mp4?alt=media&token=f62a2bed-3a98-49cf-95ec-9a8224b65015","enjoy!"));
+        addItem(new DummyItem("4","Custom Gym Tips","https://firebasestorage.googleapis.com/v0/b/hours-1a681.appspot.com/o/tutorial_vids%2Fgymtips.mp4?alt=media&token=54d7f3e1-4320-4ea1-ab46-b22bb8eb1844","Watch the Video!"));
+        addItem(new DummyItem("5","How to use Social in 24H","https://firebasestorage.googleapis.com/v0/b/hours-1a681.appspot.com/o/tutorial_vids%2Fsocial.mp4?alt=media&token=62bb71a0-0da3-4ef9-8077-58a744bb5138","Select one of the provided Templates or Create Your Own via the circular Button Thingy, the rest is simple"));
+        addItem(new DummyItem("6","How to Create a Custom Social event.","https://firebasestorage.googleapis.com/v0/b/hours-1a681.appspot.com/o/tutorial_vids%2FcustomS.mp4?alt=media&token=564c6862-e46f-4277-a035-ca07d847689a","enjoy!"));
+        addItem(new DummyItem("7","Budget $hopper Tips","https://firebasestorage.googleapis.com/v0/b/hours-1a681.appspot.com/o/tutorial_vids%2FbsTips.mp4?alt=media&token=0f4606bc-54ff-4553-b7a4-2d664c2d2554","enjoy!"));
+
+        addItem(new DummyItem("8","How to use Study in 24H","https://firebasestorage.googleapis.com/v0/b/hours-1a681.appspot.com/o/tutorial_vids%2Fstudy.mp4?alt=media&token=23c13f02-a883-4fc9-a806-6a9b76ea1e7c","Enter Your Subjects, select subjects to study hit the CHECK MARK AFTER EVERY DAY to save, the  blue button to save and close"));
+        addItem(new DummyItem("9","How to use Quick note in 24H","https://firebasestorage.googleapis.com/v0/b/hours-1a681.appspot.com/o/tutorial_vids%2Fqplan.mp4?alt=media&token=eced8d01-8592-422f-82fe-106ee78a5d1d",""));
+        addItem(new DummyItem("10","How to use Notes in 24H","https://firebasestorage.googleapis.com/v0/b/hours-1a681.appspot.com/o/tutorial_vids%2Fnotes.mp4?alt=media&token=9ff727b5-5eee-4a1f-a76f-86d74104ade7","Watch the Video!"));
+        addItem(new DummyItem("11","How to use Calendar in 24H","https://firebasestorage.googleapis.com/v0/b/hours-1a681.appspot.com/o/tutorial_vids%2Fcalendar.mp4?alt=media&token=8b8894bb-3fdc-4d35-8018-82dd1257b02c","enjoy!"));
+        addItem(new DummyItem("12","Home • 24H","https://firebasestorage.googleapis.com/v0/b/hours-1a681.appspot.com/o/tutorial_vids%2Fmain.mp4?alt=media&token=f62a2bed-3a98-49cf-95ec-9a8224b65015","enjoy!"));
+
     }
 
     private static void addItem(DummyItem item) {
